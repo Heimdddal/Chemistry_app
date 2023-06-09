be4738b6efdf30e08ca4494eb2b9f4bd7694fbbb
Влад Кантамиров <kantamirov2016@gmail.com>
2023-06-09 14:33:25 +0300

    add window registration and authorization

--- RegistrationWindow.xaml.cs
+++ RegistrationWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Chemistry_app
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для RegistrationWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegistrationWindow : Window
     {
-        public MainWindow()
+        public RegistrationWindow()
         {
             InitializeComponent();
         }
