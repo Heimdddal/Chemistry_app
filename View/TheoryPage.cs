1b9fbb33e656ef47b1aa4cf3f2b8a2855b5ec426
emil mamedov <mamedovemil366@gmail.com>
2023-06-19 18:37:41 +0300

    add new pages

--- View/TheoryPage.cs
+++ View/TheoryPage.cs
@@ -8,6 +8,10 @@ namespace Chemistry_app.View
 {
     internal class TheoryPage
     {
+        public int WordSize { get; set; }
+        public List<string> BoldWords { get; set; }
         public string Content { get; set; }
+        public List<string> BoldCombinations { get; set; }  // Добавленное поле
+        public Dictionary<string, int> WordSizes { get; set; }  // Добавленное поле
     }
 }
