a4ef6e2023041981e35f63972346935ebf6123d4
Heimdddal <dimasavolin@gmail.com>
2023-06-20 19:56:49 +0300

    Add template for Mendeleev table cell

---
+++ CustomProperties/MendeleevContentControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Chemistry_app.CustomProperties
+{
+    internal class MendeleevContentControl : ContentControl
+    {
+        public static readonly DependencyProperty AtomicNumberProperty =
+        DependencyProperty.Register("AtomicNumber", typeof(string), typeof(MendeleevContentControl));
+
+        public string AtomicNumber
+        {
+            get { return (string)GetValue(AtomicNumberProperty); }
+            set { SetValue(AtomicNumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty AtomicWeightProperty =
+        DependencyProperty.Register("AtomicWeight", typeof(string), typeof(MendeleevContentControl));
+
+        public string AtomicWeight
+        {
+            get { return (string)GetValue(AtomicWeightProperty); }
+            set { SetValue(AtomicWeightProperty, value); }
+        }
+    }
+}
